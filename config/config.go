e612d1778d00c2e16be62952daa599593881e0d8
v_vypliu <v_vypliu@tencent.com>
2023-04-26 16:01:12 +0800

    update config

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	defaultConfigPath = "./config.yaml"
+	defaultConfigPath = "./config/config.yaml"
 )
 
 type Global struct {
@@ -47,8 +47,8 @@ type Cache struct {
 }
 
 type Conf struct {
-	Etcd     []etcd.Config `mapstructure:"Etcd"`
-	Log      *log.Config   `mapstructure:"Log"`
+	Etcd     []etcd.Config `mapstructure:"etcd"`
+	Log      *log.Config   `mapstructure:"log"`
 	Global   *Global       `mapstructure:"global"`
 	Server   Server        `mapstructure:"server"`
 	Database []Database    `mapstructure:"database"`
@@ -79,7 +79,7 @@ func Init() {
 		panic(fmt.Sprintf("readConfig failed, err_msg=[%s], content=[\n%s]", err.Error(), string(localData)))
 	}
 
-	err = LoadLocal(&cfg)
+	err = vp.Unmarshal(&cfg)
 	if err != nil {
 		panic(fmt.Sprintf("LoadLocal failed, err_msg=[%s] content=[\n%s]", err.Error(), string(localData)))
 	}
@@ -138,7 +138,12 @@ func readConfig() error {
 
 // LoadLocal load config by local config file
 func LoadLocal(conf interface{}) error {
-	if err := vp.Unmarshal(conf); err != nil {
+	var ext = struct {
+		Extend interface{} `mapstructure:"extend"`
+	}{
+		Extend: conf,
+	}
+	if err := vp.Unmarshal(&ext); err != nil {
 		return fmt.Errorf("local config unmarshal failed, err_msg=[%s], content=[\n%s]", err.Error(), string(localData))
 	}
 
